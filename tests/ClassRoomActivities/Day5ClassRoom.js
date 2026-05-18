cea843df3d72b7b01696ee383cd03e75271daf62
Vignesh <vikivijay7@gmail.com>
2026-05-18 21:41:40 +0530

    Day5 Commit done

---
+++ tests/ClassRoomActivities/Day5ClassRoom.js
@@ -0,0 +1,20 @@
+let num=30;
+
+for (let i=0; i<=num;i++)
+{
+    if(i%2==1)
+        console.log("The odd numbers are : " + i)
+}
+
+let num1=25;
+
+for (let i=0;i<=num1;i++)
+{
+ if(i%2==1)
+        console.log("The odd numbers are : " + i)
+    
+    else if(i%2==0) 
+        {
+            console.log("The even numbers are : " + i)
+        }  
+}
---
+++ tests/HomeworkWeek2/ReverseString.js
@@ -0,0 +1,7 @@
+let myName ="Vignesh";
+let fname= myName.split("");
+
+for (let i=myName.length-1; i>=0; i-- )
+{
+    console.log(fname[i])
+}
