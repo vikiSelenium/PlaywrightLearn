987b731371df1a21d3cf28be435a8845fbabd71d
Vignesh <vikivijay7@gmail.com>
2026-05-20 21:13:48 +0530

    test

---
+++ tests/ClassRoomActivities/Practise.js
@@ -0,0 +1,17 @@
+function printName(name,id){
+console.log(name+" "+id)
+
+
+}
+
+
+printName(true,null)
+printName("Python",9)
+
+
+function add(empname,id){
+    return empname+ " "+id
+}
+
+
+console.log(add("TL",8))
